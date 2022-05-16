243b57c36144743a2d068c76138a39afd75fac40
Antonio Mendoza Pérez <antmendoza@gmail.com>
2022-05-16 15:09:29 +0200

    updated-version_1swfsdk

--- flyte/src/tools/jq.py
+++ flyte/src/tools/jq.py
@@ -1,12 +1,6 @@
 import jq
 
 
-def f(v, data):
-    replace = v.replace("$", "", 1).replace("{", "", 1).replace("}", "", 1)
-    first = jq.compile(replace).input(data).first()
-    return first
-
-
 class JQ:
 
     def __init__(self, expression: dict):
@@ -14,8 +8,13 @@ class JQ:
 
     def executeDict(self, data: dict):
         items = self.expression.items()
-        items_ = {k: f(v, data) for k, v in items}
+        items_ = {k: self._jqcompile(v, data) for k, v in items}
         return items_
 
     def executeString(self, data):
-        return f(self.expression, data)
+        return self._jqcompile(self.expression, data)
+
+    def _jqcompile(v, data):
+        replace = v.replace("$", "", 1).replace("{", "", 1).replace("}", "", 1)
+        first = jq.compile(replace).input(data).first()
+        return first
