9641a1956744050d973eab1c73396b810b2df0ef
Antonio Mendoza Pérez <antmendoza@gmail.com>
2022-05-16 13:00:35 +0200

    updated-version_1swfsdk

--- flyte/test/tools/test_jq.py
+++ flyte/test/tools/test_jq.py
@@ -5,11 +5,24 @@ from flyte.src.tools.jq import JQ
 
 class TestJsonPath(TestCase):
 
-    def test_jq(self):
+    def test_jq_string(self):
         expression = "${.person.name}"
         data = {
             "person": {
                 "name": "John"
             }
         }
-        self.assertEqual("John", JQ(expression).executeDict(data))
+        self.assertEqual("John", JQ(expression).executeString(data))
+
+
+    def test_jq_dict(self):
+        expression = {
+                "name": "${ .person.name }"
+              }
+        data = {
+            "person": {
+                "name": "John"
+            }
+        }
+        execute_dict = JQ(expression).executeDict(data)
+        self.assertEqual({'name': 'John'}, execute_dict)
